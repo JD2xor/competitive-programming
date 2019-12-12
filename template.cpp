251f864575552b3c564bd0630dda0d43a24ecacc
JD2xor <flere210.0323@gmail.com>
2019-12-12 23:26:37 +0900

    Modify

--- template.cpp
+++ template.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 using LL = long long;
 
-#define ALL(a) begin((a)), end((a))
-#define FOR(i, a, b) for (int(i) = (a); (i) < (b); (i)++)
-#define RFOR(i, a, b) for (int(i) = (a); (i) > (b); (i)--)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b) for (int(i) = (a); (i) > (b); --(i))
+#define SORT(x) sort(ALL((x)))
+#define RSORT(x) sort(RALL((x)))
+#define SZ(x) (int)(x).size()
 #define debug(x) cerr << #x << " : " << x << '\n'
 
 const int INF = 1e9;
 const int MOD = 1e9 + 7;
-const LL LINF = 1e16;
+const LL LINF = 1e18;
 
 int main() {
   cin.tie(0);
